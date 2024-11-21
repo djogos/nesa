ac37bb14317050b89d271018bae300a44f6cf3d4
djogos <38543724+djogos@users.noreply.github.com>
2024-11-21 21:50:16 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -69,112 +69,110 @@ def upisi_podatke(df):
                           valueInputOption='RAW', body=body).execute()
 tipovi_proizvoda = ['drvo', 'drvo lr', 'drvo db', 'pokl tegle', 'satna', 'staklo ili ambalaža']
 
-# Логика за приступ заштићеним садржајима
-def main():
-    if "logged_in" not in st.session_state or not st.session_state.logged_in:
-        login()  # Ако није пријављен, позови login()
-    else:
-        if st.sidebar.button("Logout"):
-            st.session_state.logged_in = False
-            st.session_state.username = None
-            st.rerun()  # Поново учитај апликацију
-        # Dugme za osvežavanje u sidebar-u
-        # Učitavanje podataka
-        df = citaj_podatke()
-        if st.sidebar.button("Osveži podatke"):
-            # Brisanje keša
-            st.cache_data.clear()
-            # Ponovno učitavanje podataka
-            df = citaj_podatke()
-            st.success("Podaci su osveženi!")
-            
-        #if st.sidebar.button("Zatvori aplikaciju"):
-        #    os._exit(0)
-        
-        # Kreiranje tabova
-        tab4, tab2, tab1, tab3  = st.tabs(["Prikaži stanje", "Promeni stanje", "Dodaj proizvod", "Obriši proizvod"])
-        
-        # Tab 1: Dodaj proizvod
-        with tab1:
-            st.header("Dodaj novi proizvod")
-            with st.form("novi_proizvod"):
-                naziv = st.text_input("Naziv:")
-                tip = st.selectbox("Tip:", tipovi_proizvoda)
-                cena = st.number_input("Cena:", min_value=0.0, step=0.01)
-                zalihe = st.number_input("Zalihe:", min_value=0.0, step=0.01)
-                status = 'Nepoznat'
-                napomene = st.text_input("Napomene:")
-                if st.form_submit_button("Dodaj"):
-                    if naziv in df['Naziv stavke'].values:
-                        st.error("Proizvod sa tim nazivom već postoji!")
-                    else:
-                        novi_proizvod = pd.DataFrame([[len(df) + 1, naziv, tip, cena, zalihe, status, napomene]], columns=df.columns)
-                        df = pd.concat([df, novi_proizvod], ignore_index=True)
-                        upisi_podatke(df)
-                        st.success("Proizvod dodat!")
-        
-        # Tab 2: Promeni stanje
-        with tab2:
-            st.header("Promeni stanje proizvoda")
-            with st.form("promeni_stanje"):
-                izabrani_proizvod = st.selectbox("Izaberite proizvod:", df['Naziv stavke'])
-                indeks = df[df['Naziv stavke'] == izabrani_proizvod].index[0]
-        
-                # Unos vrednosti za uvećanje/smanjenje
-                x = st.number_input("Vrednost za promenu:", min_value=0, step=1)
-        
-                # Dugmad za uvećanje i smanjenje
-                col1, col2 = st.columns(2)
-                with col1:
-                    if st.form_submit_button("Uvećaj"):
-                        df.loc[indeks, 'Zalihe'] += x
-                        upisi_podatke(df)
-                        st.success(f"Zalihe proizvoda '{izabrani_proizvod}' uvećane za {x}.")
-                with col2:
-                    if st.form_submit_button("Smanji"):
-                        if df['Zalihe'][indeks] >= x:
-                            df.loc[indeks, 'Zalihe'] -= x
-                            upisi_podatke(df)
-                            st.success(f"Zalihe proizvoda '{izabrani_proizvod}' smanjene za {x}.")
-                        else:
-                            st.error(f"Nema dovoljno zaliha proizvoda '{izabrani_proizvod}'.")
-        
-        # Tab 3: Obriši proizvod
-        with tab3:
-            st.header("Obriši proizvod")
-            # Funkcija za ažuriranje liste proizvoda u session_state
-            def update_product_list():
-                st.session_state.product_list = df['Naziv stavke'].tolist()
-            
-            # Ako session_state nema listu proizvoda, inicijalizujemo je
-            if 'product_list' not in st.session_state:
-                update_product_list()
-            izabrani_proizvod = st.selectbox("Izaberite proizvod za brisanje:", st.session_state.product_list)
-            if st.button("Obriši"):
-                df = df[df['Naziv stavke'] != izabrani_proizvod]
-                upisi_podatke(df)
-                update_product_list()
-                st.success("Proizvod obrisan!")
-                st.write(df)
-        
-        # Tab 4: Prikaži stanje
-        with tab4:
-            st.header("Stanje magacina")
-        
-            # Omogućavanje izmene DataFrame-a
-            edited_df = st.data_editor(df)
-        
-            # Dugme za potvrdu promena
-            if st.button("Potvrdi promene"):
-                # Provera da li je DataFrame izmenjen
-                if edited_df is not df:
-                    st.success("Podaci su uspešno izmenjeni!")
-                    # Upisivanje izmenjenog DataFrame-a u Google Sheet
-                    upisi_podatke(edited_df)
-                else:
-                    st.warning("Niste napravili nikakve promene.")
 
-if __name__ == "__main__":
-    main()
+if "logged_in" not in st.session_state or not st.session_state.logged_in:
+    login()  # Ако није пријављен, позови login()
+else:
+    if st.sidebar.button("Logout"):
+        st.session_state.logged_in = False
+        st.session_state.username = None
+        st.rerun()  # Поново учитај апликацију
+    # Dugme za osvežavanje u sidebar-u
+    # Učitavanje podataka
+    df = citaj_podatke()
+    if st.sidebar.button("Osveži podatke"):
+        # Brisanje keša
+        st.cache_data.clear()
+        # Ponovno učitavanje podataka
+        df = citaj_podatke()
+        st.success("Podaci su osveženi!")
+        
+    #if st.sidebar.button("Zatvori aplikaciju"):
+    #    os._exit(0)
+    
+    # Kreiranje tabova
+    tab4, tab2, tab1, tab3  = st.tabs(["Prikaži stanje", "Promeni stanje", "Dodaj proizvod", "Obriši proizvod"])
+    
+    # Tab 1: Dodaj proizvod
+    with tab1:
+        st.header("Dodaj novi proizvod")
+        with st.form("novi_proizvod"):
+            naziv = st.text_input("Naziv:")
+            tip = st.selectbox("Tip:", tipovi_proizvoda)
+            cena = st.number_input("Cena:", min_value=0.0, step=0.01)
+            zalihe = st.number_input("Zalihe:", min_value=0.0, step=0.01)
+            status = 'Nepoznat'
+            napomene = st.text_input("Napomene:")
+            if st.form_submit_button("Dodaj"):
+                if naziv in df['Naziv stavke'].values:
+                    st.error("Proizvod sa tim nazivom već postoji!")
+                else:
+                    novi_proizvod = pd.DataFrame([[len(df) + 1, naziv, tip, cena, zalihe, status, napomene]], columns=df.columns)
+                    df = pd.concat([df, novi_proizvod], ignore_index=True)
+                    upisi_podatke(df)
+                    st.success("Proizvod dodat!")
+    
+    # Tab 2: Promeni stanje
+    with tab2:
+        st.header("Promeni stanje proizvoda")
+        with st.form("promeni_stanje"):
+            izabrani_proizvod = st.selectbox("Izaberite proizvod:", df['Naziv stavke'])
+            indeks = df[df['Naziv stavke'] == izabrani_proizvod].index[0]
+    
+            # Unos vrednosti za uvećanje/smanjenje
+            x = st.number_input("Vrednost za promenu:", min_value=0, step=1)
+    
+            # Dugmad za uvećanje i smanjenje
+            col1, col2 = st.columns(2)
+            with col1:
+                if st.form_submit_button("Uvećaj"):
+                    df.loc[indeks, 'Zalihe'] += x
+                    upisi_podatke(df)
+                    st.success(f"Zalihe proizvoda '{izabrani_proizvod}' uvećane za {x}.")
+            with col2:
+                if st.form_submit_button("Smanji"):
+                    if df['Zalihe'][indeks] >= x:
+                        df.loc[indeks, 'Zalihe'] -= x
+                        upisi_podatke(df)
+                        st.success(f"Zalihe proizvoda '{izabrani_proizvod}' smanjene za {x}.")
+                    else:
+                        st.error(f"Nema dovoljno zaliha proizvoda '{izabrani_proizvod}'.")
+    
+    # Tab 3: Obriši proizvod
+    with tab3:
+        st.header("Obriši proizvod")
+        # Funkcija za ažuriranje liste proizvoda u session_state
+        def update_product_list():
+            st.session_state.product_list = df['Naziv stavke'].tolist()
+        
+        # Ako session_state nema listu proizvoda, inicijalizujemo je
+        if 'product_list' not in st.session_state:
+            update_product_list()
+        izabrani_proizvod = st.selectbox("Izaberite proizvod za brisanje:", st.session_state.product_list)
+        if st.button("Obriši"):
+            df = df[df['Naziv stavke'] != izabrani_proizvod]
+            upisi_podatke(df)
+            update_product_list()
+            st.success("Proizvod obrisan!")
+            st.write(df)
+    
+    # Tab 4: Prikaži stanje
+    with tab4:
+        st.header("Stanje magacina")
+    
+        # Omogućavanje izmene DataFrame-a
+        edited_df = st.data_editor(df)
+    
+        # Dugme za potvrdu promena
+        if st.button("Potvrdi promene"):
+            # Provera da li je DataFrame izmenjen
+            if edited_df is not df:
+                st.success("Podaci su uspešno izmenjeni!")
+                # Upisivanje izmenjenog DataFrame-a u Google Sheet
+                upisi_podatke(edited_df)
+            else:
+                st.warning("Niste napravili nikakve promene.")
+
+
 
             
